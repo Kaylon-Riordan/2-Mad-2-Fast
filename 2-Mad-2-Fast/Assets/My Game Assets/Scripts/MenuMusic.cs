14d3eafe291d2ed719fc47a9a4b61f785c67dad1
Kaylon-Riordan <kaylonriordan@gmail.com>
2024-12-15 15:11:08 +0000

    :sparkles: Added Menu Music
    Made a new script that uses bens event system on the menu canvas to play the menu music when its supposed to. Also added menu image

---
+++ 2-Mad-2-Fast/Assets/My Game Assets/Scripts/MenuMusic.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
+using static UnityEngine.ProBuilder.AutoUnwrapSettings;
+using static UnityEngine.SpriteMask;
+
+//  Learned how to use delegates for observer https://youtu.be/J01z1F-du-E
+public class MenuMusic : MonoBehaviour
+{
+    [Header("Audio Clips")]
+    [SerializeField]
+    private AudioClip menu;
+    [SerializeField]
+    private AudioClip options;
+
+    [HideInInspector]
+    public AudioSource menuSource;
+    AudioSource optionsSource;
+
+    void Start()
+    {
+        menuSource = new GameObject().AddComponent<AudioSource>();
+        optionsSource = new GameObject().AddComponent<AudioSource>();
+        Debug.Log("ASDASHGFHADGJDGHGHGHJ");
+        
+
+        AudioManager.instance.PlayMusic(menu, ref menuSource);
+        AudioManager.instance.PlayMusic(options, ref optionsSource);
+        menuSource.mute = false;
+    }
+
+    public void MenuOff()
+    {
+        menuSource.mute = true;
+    }
+    public void OptionsOn()
+    {
+        optionsSource.mute = false;
+    }
+    public void OptionsOff()
+    {
+        optionsSource.mute = true;
+    }
+}
